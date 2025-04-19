e13b993d31f89eed5af5fa90480edea4607e384d
abdumajidovismoilbek007 <abdumajidovismoilbek007@gmail.com>
2025-04-19 20:57:20 +0500

    otp input react

--- src/components/OtpInput/OtpInput.jsx
+++ src/components/OtpInput/OtpInput.jsx
@@ -1,63 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useRef, useEffect } from "react"
 
 const OtpInput = ({ value, onChange, numInputs, separator }) => {
-  const [otp, setOtp] = useState(value.split('').concat(Array(numInputs).fill('')).slice(0, numInputs));
-  const inputRefs = useRef([]);
+  const [otp, setOtp] = useState(value.split("").concat(Array(numInputs).fill("")).slice(0, numInputs))
+  const inputRefs = useRef([])
 
   useEffect(() => {
-    setOtp(value.split('').concat(Array(numInputs).fill('')).slice(0, numInputs));
-  }, [value, numInputs]);
+    setOtp(value.split("").concat(Array(numInputs).fill("")).slice(0, numInputs))
+  }, [value, numInputs])
 
   const handleChange = (element, index) => {
-    const val = element.value;
-    if (isNaN(val) && val !== '') return;
+    const val = element.value
+    if (isNaN(val) && val !== "") return
 
-    const newOtp = [...otp];
-    newOtp[index] = val;
+    const newOtp = [...otp]
+    newOtp[index] = val
 
-    setOtp(newOtp);
-    onChange(newOtp.join(''));
+    setOtp(newOtp)
+    onChange(newOtp.join(""))
 
-    if (val !== '' && index < numInputs - 1) {
-      inputRefs.current[index + 1].focus();
+    if (val !== "" && index < numInputs - 1) {
+      inputRefs.current[index + 1].focus()
     }
-  };
+  }
 
   const handleKeyDown = (e, index) => {
-    if (e.key === 'Backspace' && otp[index] === '') {
+    if (e.key === "Backspace" && otp[index] === "") {
       if (index > 0) {
-        inputRefs.current[index - 1].focus();
+        inputRefs.current[index - 1].focus()
       }
     }
-  };
+  }
 
   return (
     <div>
       {otp.map((data, index) => (
         <React.Fragment key={index}>
-          <input
-            ref={(el) => inputRefs.current[index] = el}
-            type="text"
-            maxLength="1"
-            value={data}
-            onChange={(e) => handleChange(e.target, index)}
-            onKeyDown={(e) => handleKeyDown(e, index)}
-            onFocus={(e) => e.target.select()}
-            style={{ width: '2rem', height: '2rem', textAlign: 'center', margin: '0.5rem' }}
-          />
+          <input ref={(el) => (inputRefs.current[index] = el)} type="text" maxLength="1" value={data} onChange={(e) => handleChange(e.target, index)} onKeyDown={(e) => handleKeyDown(e, index)} onFocus={(e) => e.target.select()} style={{ width: "2rem", height: "2rem", textAlign: "center", margin: "0.5rem" }} />
           {index < numInputs - 1 && separator}
         </React.Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
-OtpInput.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  numInputs: PropTypes.number.isRequired,
-  separator: PropTypes.node,
-};
-
-export default OtpInput;
+export default OtpInput
